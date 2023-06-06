74b947cf10f85e8c91cab6cddf96a028ef20451c
SooJi Kim <sjkim.suzy@gmail.com>
2023-06-06 18:40:06 -0400

    added sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 // import Page1 from './pages/Dashboard';
-import Home from './pages/Home';
+import Home from './pages/Dashboard/Dashboard';
 import DashboardPage from './pages/Dashboard';
 
 function App() {
