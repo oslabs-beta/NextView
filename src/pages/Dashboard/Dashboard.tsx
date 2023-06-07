f3bf81d8f3e73047d6f9571ba84ba238bae177f2
Eduardo Zayas <ezayasavila@gmail.com>
2023-06-07 19:36:30 -0400

    started work on fetchData()

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -42,6 +42,59 @@ const Dashboard = () => {
     //   // Process the fetched data
     // };
 
+    // const fetchData = () => {
+
+    //   let interval:number;
+    //   let unit:string;
+
+    //   switch(period){
+    //     case '1 hour' :
+    //       interval = 1;
+    //       unit = 'h';
+    //       break;
+    //     case '3 hours' :
+    //       interval = 3;
+    //       unit = 'h';
+    //       break;
+    //     case '6 hours' :
+    //       interval = 6;
+    //       unit = 'h';
+    //       break;
+    //     case '12 hours' :
+    //       interval = 12;
+    //       unit = 'h';
+    //       break;
+    //     case '24 hours' :
+    //       interval = 24;
+    //       unit = 'h';
+    //       break;
+    //     case '3 days' :
+    //       interval = 3;
+    //       unit = 'd';
+    //       break;
+    //     case '7 days' :
+    //       interval = 7;
+    //       unit = 'd';
+    //       break;
+    //     case '30 days' :
+    //       interval = 30;
+    //       unit = 'd';
+    //       break;
+    //     default:
+    //       interval = 24;
+    //       unit = 'h';
+    //   }
+
+    //   // fetch(/apps/{process.env.API_KEY}/data?interval=${interval}&unit=${unit})
+    //   fetch(`/apps/5cc036aa-e9fb-43a0-9ed7-8cafb2feb93d/data?interval=${interval}&unit=${unit}`)
+    //   .then(res => res.json())
+    //   .then(data => {
+    //       console.log('data', data);
+    //       // setData(data);
+    //   })
+    //   .catch(error => console.log('Data fetching failed', error));
+    // }
+
     const lineChartSampleData = [
       {
         name: '00:00',
