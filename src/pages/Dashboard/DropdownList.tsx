ac762b195b9761cd5d2768e5f8420f291b4346fc
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-06-07 16:12:48 -0500

    added getData func

--- src/pages/Dashboard/DropdownList.tsx
+++ src/pages/Dashboard/DropdownList.tsx
@@ -17,14 +17,73 @@ const durationList = [
 ];
 
 const DropdownList = () => {
-  const [selected, setSelected] = useState('24 hours');
+  //TODO: to replaced by React context
+  const [period, setPeriod] = useState('24 hours');
+
+  // const [data, setData] = useState({});
+
+  // const fetchData = () => {
+
+  //   let interval:number;
+  //   let unit:string;
+
+  //   switch(period){
+  //     case '1 hour' :
+  //       interval = 1;
+  //       unit = 'h';
+  //       break;
+  //     case '3 hours' :
+  //       interval = 3;
+  //       unit = 'h';
+  //       break;
+  //     case '6 hours' :
+  //       interval = 6;
+  //       unit = 'h';
+  //       break;
+  //     case '12 hours' :
+  //       interval = 12;
+  //       unit = 'h';
+  //       break;
+  //     case '24 hours' :
+  //       interval = 24;
+  //       unit = 'h';
+  //       break;
+  //     case '3 days' :
+  //       interval = 3;
+  //       unit = 'd';
+  //       break;
+  //     case '7 days' :
+  //       interval = 7;
+  //       unit = 'd';
+  //       break;
+  //     case '30 days' :
+  //       interval = 30;
+  //       unit = 'd';
+  //       break;
+  //     default:
+  //       interval = 24;
+  //       unit = 'h';
+  //   }
+
+  //   console.log(period);
+
+  //   //TODO: update setState using React context
+  //   fetch(`/apps/{process.env.API_KEY}/data?interval=${interval}&unit=${unit}`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //       console.log('data', data);
+  //       setData(data);
+  //   })
+  //   .catch(error => console.log('Data fetching failed', error));
+
+  // }
 
   return (
     <div className='fixed top-16 w-60'>
-      <Listbox value={selected} onChange={setSelected}>
+      <Listbox value={period} onChange={setPeriod}>
         <div className='relative mt-1'>
           <Listbox.Button className='relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
-            <span className='block truncate'>{selected}</span>
+            <span className='block truncate'>{period}</span>
             <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
               <BsChevronDoubleDown
                 className='h-5 w-5 text-gray-400'
