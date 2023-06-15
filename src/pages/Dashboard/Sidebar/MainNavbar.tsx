b61064883cda808d7b8c172748d5b0e58987a50c
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-06-14 21:42:13 -0500

    refactored MainNavbar

--- src/pages/Dashboard/Sidebar/MainNavbar.tsx
+++ src/pages/Dashboard/Sidebar/MainNavbar.tsx
@@ -5,72 +5,23 @@ import {
   BarGraphContext,
   BarDataItem,
 } from '../../../contexts/dashboardContexts';
+import { v4 as uuidv4 } from 'uuid';
+import PageTab from './PageTab';
 
-// TODO: Add algo to display all pages of developer's app
 function MainNavBar() {
-  const data = useContext(BarGraphContext);
-  console.log('data', data);
+  //TODO: replace with useContext
+  const pagesList = ['/api/pruducts', 'ssr/1', 'ssr/2', '/ssr/3', 'ssg/66'];
+
   return (
     <div className='flex h-screen w-44 flex-col items-center bg-slate-600 text-white'>
       <p className='mb-5 mt-10 rounded-lg px-8 py-2 font-sans text-lg font-semibold visited:text-white hover:bg-slate-500 hover:no-underline'>
         Overview
       </p>
-      {data.map((item) => (
-        <SidebarDevLink key={item.page} item={item} />
+      {pagesList.map((page) => (
+        <PageTab key={uuidv4()} page={page} />
       ))}
     </div>
   );
 }
-
-// function SidebarDevLink({ item }: { item: DevLink }) {
-//   return (
-//     <Link
-//       to={item.path}
-//       className='rounded-lg px-6 py-2 font-sans text-base font-semibold visited:text-white hover:bg-slate-500 hover:text-white hover:no-underline'
-//     >
-//       {item.key}
-//     </Link>
-//   );
-// }
-
-function SidebarDevLink({ item }: { item: BarDataItem }) {
-  return (
-    <Link
-      to={item.page}
-      className='rounded-lg px-6 py-2 font-sans text-base font-semibold visited:text-white hover:bg-slate-500 hover:text-white hover:no-underline'
-    >
-      {item.page}
-    </Link>
-  );
-}
-
-// interface DevLink {
-//   key: string;
-//   path: string;
-// }
-
-// Page links from dev app
-// const DashboardDevLinks: DevLink[] = [
-//   {
-//     key: 'fileName',
-//     path: '/fileName',
-//   },
-//   {
-//     key: 'fileName',
-//     path: '/fileName',
-//   },
-//   {
-//     key: 'fileName',
-//     path: '/fileName',
-//   },
-//   {
-//     key: 'fileName',
-//     path: '/fileName',
-//   },
-//   {
-//     key: 'fileName',
-//     path: '/fileName',
-//   },
-// ];
 
 export default MainNavBar;
