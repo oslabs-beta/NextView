5dc5ca7b8b78466f93d9b7985db16f837962118b
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-06-14 22:26:54 -0500

    updated loading

--- src/pages/Dashboard/Loading.tsx
+++ src/pages/Dashboard/Loading.tsx
@@ -2,11 +2,13 @@ const Loading = () => {
   return (
     <div
       wire:loading
-      class='fixed bottom-0 left-0 right-0 top-0 z-50 flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-gray-700 opacity-75'
+      className='fixed bottom-0 left-0 right-0 top-0 z-50 flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-gray-700 opacity-75'
     >
-      <div class='loader mb-4 h-12 w-12 rounded-full border-4 border-t-4 border-gray-200 ease-linear'></div>
-      <h2 class='text-center text-xl font-semibold text-white'>Loading...</h2>
-      <p class='w-1/3 text-center text-white'>
+      <div className='loader mb-4 h-12 w-12 rounded-full border-4 border-t-4 border-gray-200 ease-linear'></div>
+      <h2 className='text-center text-xl font-semibold text-white'>
+        Loading...
+      </h2>
+      <p className='w-1/3 text-center text-white'>
         This may take a few seconds, please don't close this page.
       </p>
     </div>
