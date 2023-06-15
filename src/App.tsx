abba286254bad62c923f087fe3fc5e6ebd3af28d
SooJi Kim <sjkim.suzy@gmail.com>
2023-06-15 18:32:04 -0400

    amended app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DashboardPage from './pages/Dashboard';
-import OverviewDisplay from './pages/Dashboard/MainDisplay/OverviewDisplay/OverviewDisplay';
 
 // TODO: typing for routes
 
@@ -10,7 +9,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route exact path='/' element={<DashboardPage />} />
+        <Route exact path='/' element={<Home />} />
         <Route path='/dashboard' element={<DashboardPage />} />
       </Routes>
     </div>
