e92fcd1f29cc0542fa0c1847dac4bde0470d8ae8
Scott Brasko <scottbrasko@gmail.com>
2023-06-16 18:43:15 -0500

    added logos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import Button from '../../components/Button';
 import logo from '../../assets/logo2.png';
+import githubLogo from '../../assets/github-mark.svg';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   // hard coded for dev, to delete
@@ -16,13 +18,52 @@ const Home = () => {
       <Navigate to='/dashboard' />
     ) : (
       <>
-        <div className='header relative z-50 flex h-12 flex-row justify-between bg-gray-200'>
-          <div id='header-left'>
-            <a href='/' target='_self'>
-              {/* <span> */}
+        <div className='header relative z-50 flex h-12 flex-row justify-between '>
+          <div
+            id='header-left'
+            className='mx-3 flex flex-row items-center justify-evenly text-sm'
+          >
+            <Link to={'/'}>
               <img src={logo} alt='nextview-logo' className='h-12 w-12'></img>
-              {/* </span> */}
-            </a>
+            </Link>
+            <div className='m-2'>
+              <Link
+                to={'/'}
+                className='font-semibold text-gray-500 hover:text-gray-800'
+              >
+                <span>Installation</span>
+              </Link>
+            </div>
+            <div className='m-2'>
+              <Link
+                to={'/'}
+                className='font-semibold text-gray-500 hover:text-gray-800'
+              >
+                <span>About</span>
+              </Link>
+            </div>
+            <div className='m-2'>
+              <Link
+                to={'/'}
+                className='font-semibold text-gray-500 hover:text-gray-800'
+              >
+                <span>Blog</span>
+              </Link>
+            </div>
+            <div className='m-2'>
+              <a
+                href='https://github.com/oslabs-beta/NextView'
+                target='_blank'
+                rel='noopener'
+                aria-label='Github'
+              >
+                <img
+                  src={githubLogo}
+                  alt='nextview-logo'
+                  className='h-6 w-6 transition duration-200 hover:scale-125'
+                ></img>
+              </a>
+            </div>
           </div>
           <div id='header-right' className='flex flex-row-reverse items-center'>
             <div className='mx-3'>
