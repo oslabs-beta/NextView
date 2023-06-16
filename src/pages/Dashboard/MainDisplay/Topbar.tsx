f1cf7a70e5d0995c8c460a8f1a1ec1a9ac337abc
Eduardo Zayas <ezayasavila@gmail.com>
2023-06-16 18:52:42 -0400

    modified toolbar to change dates onClick instead of onChange

--- src/pages/Dashboard/MainDisplay/Topbar.tsx
+++ src/pages/Dashboard/MainDisplay/Topbar.tsx
@@ -1,49 +1,32 @@
-// import * as React from 'react';
 import { useState, useContext } from 'react';
-// import DropdownList from '../MainDisplay/DropdownList';
 import logo from '../../../assets/logo2.png';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateTimeField } from '@mui/x-date-pickers/DateTimeField';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import dayjs, { Dayjs } from 'dayjs';
+// import dayjs, { Dayjs } from 'dayjs';
 import {
   StartContext,
   EndContext,
-  PeriodContext,
-  BarGraphContext,
-  OVLineChartContext,
-  PieChartContext,
-  TraceTextboxContext,
-  DurationTextboxContext,
-  LineDataItem,
-  BarDataItem,
-  PieDataItem,
-  PeriodContextType,
   StartContextType,
   EndContextType,
 } from '../../../contexts/dashboardContexts';
 
 const Topbar = () => {
-  const startContext = useContext(
+  const { setStart } = useContext(
     StartContext as React.Context<StartContextType>,
   );
-  const { start, setStart } = startContext;
+  const { setEnd } = useContext(EndContext as React.Context<EndContextType>);
 
-  const endContext = useContext(EndContext as React.Context<EndContextType>);
-  const { end, setEnd } = endContext;
+  const [startVal, setStartVal] = useState('');
+  const [endVal, setEndVal] = useState('');
 
   const handleClick = () => {
-    console.log('submit');
-    // setPeriod({interval: 30, unit: 'h'})
-    console.log({ start, end });
-    // console.log(dayjs().subtract(1, 'day').toISOString())
+    setStart(startVal);
+    setEnd(endVal);
   };
 
-  const handleDateChange = (date, fn) => {
-    fn(date.toISOString());
-    // console.log({start}, {end}, {timezone: Intl.DateTimeFormat().resolvedOptions().timeZone});
-    // console.log({ start, end });
+  const handleDateChange = (newDate, setStartOrEnd) => {
+    setStartOrEnd(newDate.toISOString());
   };
 
   return (
@@ -51,18 +34,16 @@ const Topbar = () => {
       <div>
         <div className='flex h-16 justify-end gap-x-10 bg-neutral-300 px-4'>
           <div className='flex items-center gap-x-3'>
-            {/* <p>The Past </p> */}
-            {/* <DropdownList /> */}
             <DateTimePicker
               label={'start'}
               onChange={(value) => {
-                handleDateChange(value, setStart);
+                handleDateChange(value, setStartVal);
               }}
             />
             <DateTimePicker
               label={'end'}
               onChange={(value) => {
-                handleDateChange(value, setEnd);
+                handleDateChange(value, setEndVal);
               }}
             />
             <button onClick={handleClick}>submit</button>
@@ -77,50 +58,3 @@ const Topbar = () => {
 };
 
 export default Topbar;
-
-// const handleClick = async () => {
-//   try {
-//     console.log('try');
-//     const response = await fetch(
-//       `/apps/5cc036aa-e9fb-43a0-9ed7-8cafb2feb93d/data?start=${start}&end=${end}`, {
-//         headers: {
-//           'User-Timezone': Intl.DateTimeFormat().resolvedOptions().timeZone
-//       }
-//   });
-//     const data = await response.json();
-//     console.log(data);
-//     setTraceCount(data.traceCount);
-//     setOverallAvgDuration(data.overallAvg);
-//     setBarData(data.pageAvgDurations);
-//     setPieData(data.kindAvgDurations);
-//     setLineData(data.kindAvgDurationsOverTime);
-//     // console.log(data);
-//     setIsLoading(false);
-//   } catch (error: unknown) {
-//     console.log('Data fetching failed', error);
-//   }
-// };
-
-// useEffect(() => {
-//   const fetchData = async () => {
-//     //add switch statement
-//     try {
-//       const response = await fetch(
-//         `/apps/5cc036aa-e9fb-43a0-9ed7-8cafb2feb93d/data?interval=${start}&unit=${end}`,
-//       );
-//       const data = await response.json();
-//       // console.log(data);
-//       // setTextboxData({overallAvg: data.overallAvg, traceCount: data.traceCount});
-//       setTraceCount(data.traceCount);
-//       setOverallAvgDuration(data.overallAvg);
-//       setBarData(data.pageAvgDurations);
-//       setPieData(data.kindAvgDurations);
-//       setLineData(data.kindAvgDurationsOverTime);
-//       // console.log(data);
-//       setIsLoading(false);
-//     } catch (error: unknown) {
-//       console.log('Data fetching failed', error);
-//     }
-//   };
-//   fetchData();
-// }, [start, end]);
