9ee763f3a3d938902717fb16a6c6957f59713aab
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-06-18 14:53:08 -0500

    updated page tab path

--- src/pages/Dashboard/Sidebar/PageTab.tsx
+++ src/pages/Dashboard/Sidebar/PageTab.tsx
@@ -5,7 +5,7 @@ function PageTab({ page }: { page: string }) {
 
   return (
     <Link
-      to={`/dashboard/${id}`}
+      to={`/dashboard/pages/${id}`}
       className='rounded-lg px-6 py-2 font-sans text-base font-semibold visited:text-white hover:bg-slate-500 hover:text-white hover:no-underline'
     >
       {page}
