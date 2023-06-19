9a074f2500c5725d618778acb11a4c1bad1c2b1e
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-06-18 19:08:56 -0500

    refactored routing

--- src/pages/Dashboard/MainDisplay/MainDisplay.tsx
+++ src/pages/Dashboard/MainDisplay/MainDisplay.tsx
@@ -1,17 +1,13 @@
 import { useState, useContext } from 'react';
 import Topbar from './Topbar';
-// import AppsListDisplay from './AppsListDisplay/AppsListDisplay';
 import OverviewDisplay from './OverviewDisplay/OverviewDisplay';
 import { PageContext } from '../../../contexts/dashboardContexts';
-
-//TODO: add logic to display PageDisplay & SettingsDisplay
 import PageDisplay from './PageDisplay/PageDisplay';
-// import SettingsDisplay from './SettingsDisplay/SettingsDisplay';
 import { Routes, Route } from 'react-router-dom';
-import Home from '../../Home';
+// import AppsListDisplay from './AppsListDisplay/AppsListDisplay';
+// import SettingsDisplay from './SettingsDisplay/SettingsDisplay';
 
 const MainDisplay = ({ overviewData, pageData, setStart, setEnd }) => {
-  //TODO: replace with useContext
   const { pageView } = useContext(PageContext);
   // console.log('pageData-MainDisplay', pageData);
   console.log('pageView', pageView);
@@ -20,8 +16,11 @@ const MainDisplay = ({ overviewData, pageData, setStart, setEnd }) => {
     <div className='h-screen w-[calc(100%-216px)]'>
       <Topbar setStart={setStart} setEnd={setEnd} />
       <Routes>
-        <Route path='/' element={<OverviewDisplay data={data} />} />
-        <Route path='/page/:pageId' element={<PageDisplay />} />
+        <Route
+          path='/'
+          element={<OverviewDisplay overviewData={overviewData} />}
+        />
+        <Route path='/page/:id' element={<PageDisplay pageData={pageData} />} />
       </Routes>
       {pageView ? (
         <PageDisplay pageData={pageData} />
