5ca73ff4dc9cc406dda9d905784259b98ac9adda
Scott Brasko <scottbrasko@gmail.com>
2023-06-21 14:12:56 -0500

    fixed firefox scrolling issue

--- src/pages/Dashboard/MainDisplay/PageDisplay/Table.tsx
+++ src/pages/Dashboard/MainDisplay/PageDisplay/Table.tsx
@@ -6,21 +6,23 @@ const Table = ({ overallPageData }) => {
     tableData.push(
       <tr key={overallPageData[i].Name} className='text-left text-sm'>
         <td
-          className='w-1/4 max-w-0 overflow-x-auto border-b border-r p-3'
+          className='w-1/3 max-w-[30px] overflow-x-auto border-b border-r p-2'
           key={uuidv4()}
         >
-          {overallPageData[i].Name}
+          <div className='w-full overflow-auto pb-2'>
+            {overallPageData[i].Name}
+          </div>
         </td>
-        <td className='border-b border-r p-3' key={uuidv4()}>
+        <td className='w-28 border-b border-r p-3' key={uuidv4()}>
           {overallPageData[i]['Avg. duration (ms)'] + 'ms'}
         </td>
-        <td className='border-b border-r p-3' key={uuidv4()}>
+        <td className='w-28 border-b border-r p-3' key={uuidv4()}>
           {overallPageData[i].Kind}
         </td>
-        <td className='border-b border-r p-3' key={uuidv4()}>
+        <td className='w-28 border-b border-r p-3' key={uuidv4()}>
           {overallPageData[i]['Total no. of traces']}
         </td>
-        <td className='border-b p-3' key={uuidv4()}>
+        <td className='w-24 border-b p-3' key={uuidv4()}>
           {overallPageData[i]['Total no. of executions']}
         </td>
       </tr>,
@@ -31,11 +33,11 @@ const Table = ({ overallPageData }) => {
     <table className='w-full overflow-hidden rounded-xl bg-white text-left drop-shadow-sm'>
       <thead className='border-b bg-secondary'>
         <tr key={'header'} className='text-sm text-white'>
-          <th className='px-4 py-4 '>Name</th>
-          <th className='px-4 py-4'>Avg. Duration</th>
-          <th className='px-4 py-4'>Kind</th>
-          <th className='px-4 py-4'>No. of Traces</th>
-          <th className='px-4 py-4'>No. of Executions</th>
+          <th className='px-1 py-4 '>Name</th>
+          <th className='px-1 py-4'>Avg. Duration</th>
+          <th className='px-1 py-4'>Kind</th>
+          <th className='px-1 py-4'>No. of Traces</th>
+          <th className='px-1 py-4'>No. of Executions</th>
         </tr>
       </thead>
       <tbody className='text-sm'>{tableData}</tbody>
