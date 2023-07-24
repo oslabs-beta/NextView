df498183d2cd7a8544d9b735adcd5d0923952df9
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-07-23 22:40:31 -0500

    replace Link with NavLink in PageTab

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,11 +1,9 @@
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  return localStorage.getItem('loggedIn') ? (
-    children
-  ) : (
-    <Navigate to='/' replace />
-  );
+  const isLoggedIn = localStorage.getItem('loggedIn') === 'true';
+
+  return isLoggedIn ? children : <Navigate to='/' replace />;
 };
 
 export default ProtectedRoute;
--- src/pages/Dashboard/Sidebar/PageTab.tsx
+++ src/pages/Dashboard/Sidebar/PageTab.tsx
@@ -1,26 +1,22 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { PageContext } from '../../../contexts/dashboardContexts';
 import { useContext } from 'react';
 
 function PageTab({ pageSelection }) {
   const { page, setPage } = useContext(PageContext);
 
-  return page !== undefined && page._id === pageSelection._id ? (
-    <Link
+  return (
+    <NavLink
       onClick={() => setPage(pageSelection)}
       to={`/dashboard/page/${pageSelection._id}`}
-      className='ml-3 mr-3 w-11/12 rounded border border-t-0 bg-gray-200 px-6 py-2 text-left text-sm font-semibold drop-shadow-sm'
+      className={({ isActive }) =>
+        isActive
+          ? 'ml-3 mr-3 w-11/12 rounded border border-t-0 bg-gray-200 px-6 py-2 text-left text-sm font-semibold drop-shadow-sm'
+          : 'ml-3 mr-3 w-11/12 rounded border-b px-6 py-2 text-left text-sm hover:bg-gray-100'
+      }
     >
       {pageSelection.page}
-    </Link>
-  ) : (
-    <Link
-      onClick={() => setPage(pageSelection)}
-      to={`/dashboard/page/${pageSelection._id}`}
-      className='ml-3 mr-3 w-11/12 rounded border-b px-6 py-2 text-left text-sm hover:bg-gray-100'
-    >
-      {pageSelection.page}
-    </Link>
+    </NavLink>
   );
 }
 
