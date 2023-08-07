2d74bd6a291093a95a6f5c5dff3aeff76f775824
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-08-06 21:32:17 -0500

    refactor protectedRoute

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,12 +1,9 @@
 import { Navigate } from 'react-router-dom';
-import { useContext } from 'react';
-import { UserContext } from './contexts/userContexts';
 
-const ProtectedRoute = ({ children }) => {
-  const { username } = useContext(UserContext);
-  const isLoggedIn = JSON.parse(localStorage.getItem('user')) === username;
+const ProtectedRoute: React.FC = ({ children }) => {
+  const storedUser = JSON.parse(localStorage.getItem('user'));
 
-  return isLoggedIn ? children : <Navigate to='/' replace />;
+  return storedUser ? children : <Navigate to='/' replace />;
 };
 
 export default ProtectedRoute;
