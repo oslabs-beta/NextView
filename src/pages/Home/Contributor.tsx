fb3dcdf5101b789b57d705f2c964e9b9be587e53
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2023-08-13 19:07:31 -0500

    modulzied Contributors

---
+++ src/pages/Home/Contributor.tsx
@@ -0,0 +1,45 @@
+import linkedin from '../../assets/linked-in.png';
+import github from '../../assets/github-mark.svg';
+
+interface ContributorProps {
+  name: string;
+  image: string;
+  linkedinLink: string;
+  githubLink: string;
+}
+
+const Contributor: React.FC<ContributorProps> = ({
+  name,
+  image,
+  linkedinLink,
+  githubLink,
+}) => {
+  <div className='col-span-10 flex flex-col items-center gap-3 drop-shadow-md sm:col-span-5 lg:col-span-2'>
+    <img src={image} className='max-h-64 drop-shadow-xl' />
+    <div className='flex flex-col whitespace-nowrap'>
+      <span className='text-xl font-bold '>{name}</span>
+      <span className='text-l'>Software Engineer</span>
+    </div>
+    <div className='flex gap-4'>
+      <a
+        href={linkedinLink}
+        target='_blank'
+        rel='noopener'
+        aria-label='linkedin'
+      >
+        <img
+          src={linkedin}
+          className='h-7 transition duration-200 hover:scale-105'
+        />
+      </a>
+      <a href={githubLink} target='_blank' rel='noopener' aria-label='Github'>
+        <img
+          src={github}
+          className='h-7 transition duration-200 hover:scale-105'
+        />
+      </a>
+    </div>
+  </div>;
+};
+
+export default Contributor;
--- src/pages/Home/Contributors.tsx
+++ src/pages/Home/Contributors.tsx
@@ -3,11 +3,44 @@ import eduardo from '../../assets/eduardo.png';
 import evram from '../../assets/evram.png';
 import scott from '../../assets/scott.png';
 import sooji from '../../assets/sooji.png';
-import linkedin from '../../assets/linked-in.png';
-import github from '../../assets/github-mark.svg';
 import hands from '../../assets/hands.png';
+import Contributor from './Contributor';
+import { v4 as uuid } from 'uuid';
 
-const Contributors = () => {
+const contributorsData = [
+  {
+    name: 'Eduardo Zayas',
+    image: eduardo,
+    linkedinLink: 'https://www.linkedin.com/in/eduardo-zayas-avila/',
+    githubLink: 'https://github.com/eza16',
+  },
+  {
+    name: 'Evram Dawd',
+    image: evram,
+    linkedinLink: 'https://www.linkedin.com/in/evram-d-905a3a2b/',
+    githubLink: 'https://github.com/evramdawd',
+  },
+  {
+    name: 'Kinski (Jiaxin) Wu',
+    image: kinski,
+    linkedinLink: 'https://www.linkedin.com/in/kinskiwu/',
+    githubLink: 'https://github.com/kinskiwu',
+  },
+  {
+    name: 'Scott Brasko',
+    image: scott,
+    linkedinLink: 'https://www.linkedin.com/in/scott-brasko/',
+    githubLink: 'https://github.com/Scott-Brasko',
+  },
+  {
+    name: 'SooJi Kim',
+    image: sooji,
+    linkedinLink: 'https://www.linkedin.com/in/sooji-suzy-kim/',
+    githubLink: 'https://github.com/sjk06',
+  },
+];
+
+const Contributors: React.FC = () => {
   return (
     <footer className='border-t bg-white px-4 pb-28 pt-20 sm:px-[max((100vw-1500px)/2,48px)]'>
       <div className='flex items-center gap-2'>
@@ -15,162 +48,15 @@ const Contributors = () => {
         <img src={hands} className='h-12' />
       </div>
       <div className='mt-12 grid grid-cols-10 gap-6 text-center'>
-        {/* TODO: Modularize these profiles into component */}
-        <div className='col-span-10 flex flex-col items-center gap-3 drop-shadow-md sm:col-span-5 lg:col-span-2'>
-          <img src={eduardo} className='max-h-64 drop-shadow-xl' />
-          <div className='flex flex-col whitespace-nowrap'>
-            <span className='text-xl font-bold '>Eduardo Zayas</span>
-            <span className='text-l'>Software Engineer</span>
-          </div>
-          <div className='flex gap-4'>
-            <a
-              href='https://www.linkedin.com/in/eduardo-zayas-avila/'
-              target='_blank'
-              rel='noopener'
-              aria-label='linkedin'
-            >
-              <img
-                src={linkedin}
-                className='h-7 transition duration-200 hover:scale-105'
-              />
-            </a>
-            <a
-              href='https://github.com/eza16'
-              target='_blank'
-              rel='noopener'
-              aria-label='Github'
-            >
-              <img
-                src={github}
-                className='h-7 transition duration-200 hover:scale-105'
-              />
-            </a>
-          </div>
-        </div>
-        <div className='col-span-10 flex flex-col items-center gap-3 drop-shadow-md sm:col-span-5 lg:col-span-2'>
-          <img src={evram} className='max-h-64 drop-shadow-md' />
-          <div className='flex flex-col whitespace-nowrap'>
-            <span className='text-xl font-bold '>Evram Dawd</span>
-            <span className='text-l'>Software Engineer</span>
-          </div>
-          <div className='flex gap-4'>
-            <a
-              href='https://www.linkedin.com/in/evram-d-905a3a2b/'
-              target='_blank'
-              rel='noopener'
-              aria-label='linkedin'
-            >
-              <img
-                src={linkedin}
-                className='h-7 transition duration-200 hover:scale-105'
-              />
-            </a>
-            <a
-              href='https://github.com/evramdawd'
-              target='_blank'
-              rel='noopener'
-              aria-label='Github'
-            >
-              <img
-                src={github}
-                className='h-7 transition duration-200 hover:scale-105'
-              />
-            </a>
-          </div>
-        </div>
-        <div className='col-span-10 flex flex-col items-center gap-3 drop-shadow-md sm:col-span-5 lg:col-span-2'>
-          <img src={kinski} className='max-h-64 drop-shadow-md' />
-          <div className='flex flex-col whitespace-nowrap'>
-            <span className='text-xl font-bold '>Kinski (Jiaxin) Wu</span>
-            <span className='text-l'>Software Engineer</span>
-          </div>
-          <div className='flex gap-4'>
-            <a
-              href='https://www.linkedin.com/in/kinskiwu/'
-              target='_blank'
-              rel='noopener'
-              aria-label='linkedin'
-            >
-              <img
-                src={linkedin}
-                className='h-7 transition duration-200 hover:scale-105'
-              />
-            </a>
-            <a
-              href='https://github.com/kinskiwu'
-              target='_blank'
-              rel='noopener'
-              aria-label='Github'
-            >
-              <img
-                src={github}
-                className='h-7 transition duration-200 hover:scale-105'
-              />
-            </a>
-          </div>
-        </div>
-        <div className='col-span-10 flex flex-col items-center gap-3 drop-shadow-md sm:col-span-5 lg:col-span-2'>
-          <img src={scott} className='max-h-64 drop-shadow-md' />
-          <div className='flex flex-col whitespace-nowrap'>
-            <span className='text-xl font-bold '>Scott Brasko</span>
-            <span className='text-l'>Software Engineer</span>
-          </div>
-          <div className='flex gap-4'>
-            <a
-              href='https://www.linkedin.com/in/scott-brasko/'
-              target='_blank'
-              rel='noopener'
-              aria-label='linkedin'
-            >
-              <img
-                src={linkedin}
-                className='h-7 transition duration-200 hover:scale-105'
-              />
-            </a>
-            <a
-              href='https://github.com/Scott-Brasko'
-              target='_blank'
-              rel='noopener'
-              aria-label='Github'
-            >
-              <img
-                src={github}
-                className='h-7 transition duration-200 hover:scale-105'
-              />
-            </a>
-          </div>
-        </div>
-        <div className='col-span-10 flex flex-col items-center gap-3 drop-shadow-md sm:col-span-10 lg:col-span-2'>
-          <img src={sooji} className='max-h-64 drop-shadow-md' />
-          <div className='flex flex-col whitespace-nowrap'>
-            <span className='text-xl font-bold '>SooJi Kim</span>
-            <span className='text-l'>Software Engineer</span>
-          </div>
-          <div className='flex gap-4'>
-            <a
-              href='https://www.linkedin.com/in/sooji-suzy-kim/'
-              target='_blank'
-              rel='noopener'
-              aria-label='linkedin'
-            >
-              <img
-                src={linkedin}
-                className='h-7 transition duration-200 hover:scale-105'
-              />
-            </a>
-            <a
-              href='https://github.com/sjk06'
-              target='_blank'
-              rel='noopener'
-              aria-label='Github'
-            >
-              <img
-                src={github}
-                className='h-7 transition duration-200 hover:scale-105'
-              />
-            </a>
-          </div>
-        </div>
+        {contributorsData.map((contributor) => (
+          <Contributor
+            key={uuid()}
+            name={contributor.name}
+            image={contributor.image}
+            linkedinLink={contributor.linkedinLink}
+            githubLink={contributor.githubLink}
+          />
+        ))}
       </div>
     </footer>
   );
