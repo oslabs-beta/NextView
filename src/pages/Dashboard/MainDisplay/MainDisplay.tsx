52cc42a93837e76d24ffd4f68756b3b6bbbb0e16
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-01-22 17:05:44 -0600

    uodated props types in maindisplay

--- src/pages/Dashboard/MainDisplay/MainDisplay.tsx
+++ src/pages/Dashboard/MainDisplay/MainDisplay.tsx
@@ -3,8 +3,24 @@ import OverviewDisplay from './OverviewDisplay/OverviewDisplay';
 import PageDisplay from './PageDisplay/PageDisplay';
 import { Routes, Route } from 'react-router-dom';
 import NotFound from '../../NotFound/NotFound';
+import {
+  OverviewDataType,
+  PageDataType,
+} from '../../../types/ComponentPropTypes';
 
-const MainDisplay = ({ overviewData, pageData, setStart, setEnd }) => {
+interface MainDisplayProps {
+  overviewData: OverviewDataType;
+  pageData: PageDataType;
+  setStart: (value: string) => void;
+  setEnd: (value: string) => void;
+}
+
+const MainDisplay: React.FC<MainDisplayProps> = ({
+  overviewData,
+  pageData,
+  setStart,
+  setEnd,
+}) => {
   return (
     <div className='relative w-full flex-col'>
       <Topbar setStart={setStart} setEnd={setEnd} overviewData={overviewData} />
