3f3d428061bc07ac9de83e94cea070306204e585
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-01-22 18:01:01 -0600

    updated type for protectedrouteprops & refactored component to handle edge case

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,9 +1,23 @@
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute: React.FC = ({ children }) => {
-  const storedUser = JSON.parse(localStorage.getItem('user'));
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
 
-  return storedUser ? children : <Navigate to='/' replace />;
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  let storedUser;
+  try {
+    storedUser = JSON.parse(localStorage.getItem('user') || 'null');
+  } catch (error) {
+    console.error('Error parsing user from localStorage', error);
+    storedUser = null;
+  }
+
+  if (storedUser) {
+    return <>{children}</>;
+  }
+
+  return <Navigate to='/' replace />;
 };
 
 export default ProtectedRoute;
