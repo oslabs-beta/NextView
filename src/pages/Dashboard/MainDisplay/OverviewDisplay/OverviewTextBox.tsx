4ffd0c3b06256ec93062fcd00e4298618b1ef817
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-01-25 16:09:09 -0600

    renamed textbox components for overview and page, incorporate React.memo

---
+++ src/pages/Dashboard/MainDisplay/OverviewDisplay/OverviewTextBox.tsx
@@ -0,0 +1,26 @@
+import Box from '../../../../components/Box';
+import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
+
+interface TextboxProps {
+  traceCount: number;
+  overallAvg: number;
+}
+
+const Textbox: React.FC<TextboxProps> = React.memo(
+  ({ traceCount, overallAvg }) => {
+    const boxData = [
+      { title: 'Average Page Load Duration', data: overallAvg },
+      { title: 'Total No. of Traces', data: traceCount },
+    ];
+    return (
+      <div className='col-span-12 flex flex-row justify-center gap-8 text-center font-semibold 3xl:col-span-2 3xl:flex-col'>
+        {boxData.map(({ title, data }) => (
+          <Box key={uuidv4()} title={title} data={data} />
+        ))}
+      </div>
+    );
+  },
+);
+
+export default Textbox;
---
+++ src/pages/Dashboard/MainDisplay/PageDisplay/PageTextBox.tsx
@@ -0,0 +1,27 @@
+import Box from '../../../../components/Box';
+import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
+
+interface TextboxProps {
+  overallAvg: number;
+  traceCount: number;
+}
+
+const Textbox: React.FC<TextboxProps> = React.memo(
+  ({ traceCount, overallAvg }) => {
+    const boxData = [
+      { title: 'Average Page Load Duration', data: overallAvg },
+      { title: 'Total No. of Traces', data: traceCount },
+    ];
+
+    return (
+      <div className='col-span-12 ml-auto mr-auto flex w-full flex-row justify-center gap-8 text-center font-semibold xl:col-span-4'>
+        {boxData.map(({ title, data }) => (
+          <Box key={uuidv4()} title={title} data={data} />
+        ))}
+      </div>
+    );
+  },
+);
+
+export default Textbox;
